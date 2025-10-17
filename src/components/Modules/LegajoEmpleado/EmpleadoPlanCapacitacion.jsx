7a84611d724d18bf9d5f45ce6d9f049a4362ee79
Ignacio Ferrari <iferrari@escorial.com.ar>
2025-10-17 14:53:28 -0300

    Se agrego un buscador en la solapa de legajo empleado y se hicieron cambios esteticos

--- src/components/Modules/LegajoEmpleado/EmpleadoPlanCapacitacion.jsx
+++ src/components/Modules/LegajoEmpleado/EmpleadoPlanCapacitacion.jsx
@@ -9,20 +9,20 @@ function EmpleadoPlanCapacitacion(props) {
                     {
                             props.propuesta.plan_capacitacion?.map((item, index) => (
                                 <div key={index} style={{marginBottom: '10px'}} className="propuesta">
-                                    <div className="">
+                                    <div className="propuesta-capacitacion-t">
                                         <div className="propuesta-capacitacion-titulo">Area mejora: </div>
                                         <div className="propuesta-capacitacion-texto">{item.area_mejora}</div>
                                     </div>
                                     <div className="">
-                                        <div className="propuesta-capacitacion-titulo">Brecha identificada: </div>
+                                        <div className="propuesta-capacitacion-subtitulo">Brecha identificada: </div>
                                         <div className="propuesta-capacitacion-texto">{item.brecha_identificada}</div>
                                     </div>
                                     <div className="">
-                                        <div className="propuesta-capacitacion-titulo">Objetivo capacitacion: </div>
+                                        <div className="propuesta-capacitacion-subtitulo">Objetivo capacitacion: </div>
                                         <div className="propuesta-capacitacion-texto">{item.objetivo_capacitacion}</div>
                                     </div>
                                     <div>
-                                        <div className="propuesta-capacitacion-titulo">Acciones recomendadas: </div>
+                                        <div className="propuesta-capacitacion-subtitulo">Acciones recomendadas: </div>
                                         <div className="propuesta-capacitacion-texto">
                                         {
                                         item.acciones_recomendadas?.map((accion) => (
@@ -32,7 +32,7 @@ function EmpleadoPlanCapacitacion(props) {
                                         </div>
                                     </div>
                                     <div className="">
-                                        <div className="propuesta-capacitacion-titulo">Plazo sugerido: </div>
+                                        <div className="propuesta-capacitacion-subtitulo">Plazo sugerido: </div>
                                         <div className="propuesta-capacitacion-texto">{item.plazo_sugerido}</div>
                                     </div>
                                 </div>
